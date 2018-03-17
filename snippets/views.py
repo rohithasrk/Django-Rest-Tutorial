2b25df3f0fac9289bceee211602a982ac0f89413
Rohith Asrk <rohith.asrk@gmail.com>
2018-03-17 19:10:52 +0530

    Add highlight option, auth.User models and views

--- snippets/views.py
+++ snippets/views.py
@@ -1,9 +1,10 @@
 from snippets.models import Snippet
-from snippets.serializers import SnippetSerializer
+from snippets.serializers import SnippetSerializer, UserSerializer
+from django.contrib.auth.models import User
 from django.http import Http404
 from rest_framework.views import APIView
 from rest_framework.response import Response
-from rest_framework import status
+from rest_framework import generics
 
 
 class SnippetList(generics.ListCreateAPIView):
@@ -14,3 +15,8 @@ class SnippetList(generics.ListCreateAPIView):
 class SnippetDetail(generics.RetrieveUpdateDestroyAPIView):
     queryset = Snippet.objects.all()
     serializer_class = SnippetSerializer
+
+
+class UserList(generics.ListAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer

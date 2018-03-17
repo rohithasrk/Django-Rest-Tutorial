dc5d02c139fd651f9f0213e28a4aea3feb840fcd
Rohith Asrk <rohith.asrk@gmail.com>
2018-03-17 18:38:39 +0530

    Move to more generic class based views

--- snippets/views.py
+++ snippets/views.py
@@ -6,31 +6,11 @@ from rest_framework.response import Response
 from rest_framework import status
 
 
-class SnippetList(mixins.ListModelMixin,
-                  mixins.CreateModelMixin,
-                  generics.GenericAPIView):
+class SnippetList(generics.ListCreateAPIView):
     queryset = Snippet.objects.all()
     serializer_class = SnippetSerializer
 
-    def get(self, request, format=None):
-        return self.list(request, *args, **kwargs)
 
-    def post(self, request, format=None):
-        return self.create(request, *args, **kwargs)
-
-
-class SnippetDetail(mixins.RetrieveModelMixin,
-                    mixins.UpdateModelMixin,
-                    mixins.DestroyModelMixin,
-                    generics.GenericAPIView):
+class SnippetDetail(generics.RetrieveUpdateDestroyAPIView):
     queryset = Snippet.objects.all()
     serializer_class = SnippetSerializer
-
-    def get(self, request, pk, format=None):
-        return self.retrieve(request, *args, **kwargs)
-
-    def put(self, request, pk, format=None):
-        return self.update(request, *args, **kwargs)
-
-    def delete(self, request, pk, format=None):
-        return self.destroy(request, *args, **kwargs)

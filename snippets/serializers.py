384166655ecd070de368df6ed03cd01d140fd815
Rohith Asrk <rohith.asrk@gmail.com>
2018-03-20 12:27:00 +0530

    Add SnippetHighlight, HyperLinks in the API

--- snippets/serializers.py
+++ snippets/serializers.py
@@ -5,15 +5,16 @@ from snippets.models import Snippet, LANGUAGE_CHOICES, STYLE_CHOICES
 
 class SnippetSerializer(serializers.ModelSerializer):
     owner = serializers.ReadOnlyField(source='owner.username')
+    highlight = serializers.HyperlinkedIdentityField(view_name='snippet-highlight', format='html')
 
     class Meta:
         model = Snippet
-        fields = ('id', 'title', 'owner', 'code', 'linenos', 'language', 'style')
+        fields = ('url', 'id', 'highlight', 'title', 'owner', 'code', 'linenos', 'language', 'style')
 
 
 class UserSerializer(serializers.ModelSerializer):
-    snippets = serializers.PrimaryKeyRelatedField(many=True, queryset=Snippet.objects.all())
+    snippets = serializers.HyperlinkedIdentityField(many=True, view_name='snippet-detail', read_only=True)
 
     class Meta:
         model = User
-        fields = ('id', 'username', 'snippets')
+        fields = ('url', 'id', 'username', 'snippets')
--- snippets/views.py
+++ snippets/views.py
@@ -1,11 +1,10 @@
 from snippets.models import Snippet
 from snippets.serializers import SnippetSerializer, UserSerializer
 from django.contrib.auth.models import User
-from django.http import Http404
-from rest_framework.views import APIView
+from rest_framework import generics, permissions, renderers
+from rest_framework.decorators import api_view
 from rest_framework.response import Response
-from rest_framework import generics
-from rest_framework import permissions
+from rest_framework.reverse import reverse
 from snippets.permissions import IsOwnerOrReadOnly
 
 
@@ -33,3 +32,20 @@ class UserList(generics.ListAPIView):
 class UserDetail(generics.RetrieveAPIView):
     queryset = User.objects.all()
     serializer_class = UserSerializer
+
+
+@api_view(['GET'])
+def api_root(request, format=None):
+    return Response({
+        'users': reverse('user-list', request=request, format=format),
+        'snippets': reverse('snippet-list', request=request, format=format)
+    })
+
+
+class SnippetHighlight(generics.GenericAPIView):
+    queryset = Snippet.objects.all()
+    renderer_classes = (renderers.StaticHTMLRenderer,)
+
+    def get(self, request, *args, **kwargs):
+        snippet = self.get_object()
+        return Response(snippet.highlighted)
